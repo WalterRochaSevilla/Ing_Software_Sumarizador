e1e8e212648a1f47a57988b79afe3bb69074bf30
Walter Rocha <walterrochasevilla@gmail.com>
2024-08-29 09:53:21 -0400

    Mostrar Estado

---
+++ src/ciudad.js
@@ -0,0 +1,4 @@
+function ciudad(a){
+    return a;
+}
+export default ciudad;
--- src/sumarizador.test.js
+++ src/sumarizador.test.js
@@ -1,7 +1,8 @@
-import sumarizador from "./sumarizador";
+//import sumarizador from "./sumarizador";
 import mostrador_items from "./items";
 import precio_neto from "./precio_neto";
 import precio from "./precio";
+import ciudad from "./ciudad";
 
 describe("Sumarizador", () => {
     it("deberia devolver Precio", () => {
@@ -13,5 +14,8 @@ describe("Sumarizador", () => {
     it("deberia devolver precio neto", () => {
       expect(precio_neto(500, 10)).toEqual(5000);
     });
+    it("deberia devolver ciudad", () => {
+        expect(ciudad("NV")).toEqual("NV");
+      });
   });
   
